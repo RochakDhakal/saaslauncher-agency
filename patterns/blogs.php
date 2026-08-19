0d52059de2905328510e9bbd5ead6d589e448c82
RochakDhakal <rochakdhakal@gmail.com>
2026-08-19 16:56:19 +0545

    UPDATE: cta-translated

--- patterns/blogs.php
+++ patterns/blogs.php
@@ -81,7 +81,11 @@
 
 <!-- wp:query-no-results -->
 <!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results."} -->
-<p>Oops! Blogs Not Found</p>
+<p>
+<?php
+	esc_html_e( 'Oops! Blogs Not Found', 'saaslauncher-agency' );
+?>
+</p>
 <!-- /wp:paragraph -->
 <!-- /wp:query-no-results --></div>
 <!-- /wp:query --></div>
--- patterns/cta.php
+++ patterns/cta.php
@@ -5,31 +5,34 @@
  * Slug: saaslauncher-agency/cta
  * Categories:  saaslauncher-agency-patterns
  */
-
-?>
+$saaslauncher_agency_url        = trailingslashit( get_stylesheet_directory_uri() );
+	$saaslauncher_agency_images = array(
+		$saaslauncher_agency_url . 'assets/images/cta.png',
+	);
+	?>
 
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|40","right":"var:preset|spacing|40"},"margin":{"top":"0","bottom":"0"},"blockGap":"0"}},"gradient":"gradient-3","layout":{"type":"constrained","contentSize":"850px"}} -->
 <div class="wp-block-group has-gradient-3-gradient-background has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--40)"><!-- wp:heading {"style":{"typography":{"textAlign":"center"}}} -->
-<h2 class="wp-block-heading has-text-align-center">Ready to build something exceptional?</h2>
+<h2 class="wp-block-heading has-text-align-center"><?php esc_html_e( 'Ready to build something exceptional?', 'saaslauncher-agency' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:group {"layout":{"type":"constrained","contentSize":"575px"}} -->
 <div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"textAlign":"center"},"spacing":{"margin":{"top":"var:preset|spacing|50"}}}} -->
-<p class="has-text-align-center" style="margin-top:var(--wp--preset--spacing--50)">Let's create a digital experience that helps your business stand out and grow.</p>
+<p class="has-text-align-center" style="margin-top:var(--wp--preset--spacing--50)"><?php esc_html_e( "Let's create a digital experience that helps your business stand out and grow.", 'saaslauncher-agency' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:buttons {"style":{"spacing":{"margin":{"top":"var:preset|spacing|60"}}},"layout":{"type":"flex","justifyContent":"center"}} -->
 <div class="wp-block-buttons" style="margin-top:var(--wp--preset--spacing--60)"><!-- wp:button {"className":"is-style-button-with-uparrow-icon"} -->
-<div class="wp-block-button is-style-button-with-uparrow-icon"><a class="wp-block-button__link wp-element-button">Start Your Project</a></div>
+<div class="wp-block-button is-style-button-with-uparrow-icon"><a class="wp-block-button__link wp-element-button"><?php esc_html_e( 'Start Your Project', 'saaslauncher-agency' ); ?></a></div>
 <!-- /wp:button -->
 
 <!-- wp:button {"backgroundColor":"transparent","textColor":"light-color","className":"is-style-fill","style":{"elements":{"link":{"color":{"text":"var:preset|color|light-color"}}},"border":{"width":"1px"}},"borderColor":"border-color"} -->
-<div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-light-color-color has-transparent-background-color has-text-color has-background has-link-color has-border-color has-border-color-border-color wp-element-button" style="border-width:1px">Book a Consultation</a></div>
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-light-color-color has-transparent-background-color has-text-color has-background has-link-color has-border-color has-border-color-border-color wp-element-button" style="border-width:1px"><?php esc_html_e( 'Book a Consultation', 'saaslauncher-agency' ); ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons -->
 
 <!-- wp:paragraph {"style":{"typography":{"textAlign":"center"},"spacing":{"margin":{"top":"var:preset|spacing|40"}}}} -->
-<p class="has-text-align-center" style="margin-top:var(--wp--preset--spacing--40)">Average reply time: 4 hours · Projects from $25k</p>
+<p class="has-text-align-center" style="margin-top:var(--wp--preset--spacing--40)"><?php esc_html_e( 'Average reply time: 4 hours · Projects from $25k', 'saaslauncher-agency' ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
